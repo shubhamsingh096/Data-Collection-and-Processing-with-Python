6a3febff95c3d9d3d8db8aa16cf1f6b1974fecf3
Shubham Kumar Singh <67189614+shubhamsingh096@users.noreply.github.com>
2020-07-10 17:29:28 +0530

    Update Course_3_Assessment_1.py

--- Course_3_Assessment_1.py
+++ Course_3_Assessment_1.py
@@ -1,4 +1,3 @@
-n [3]:
 #1) The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
 
 nested = [['dog', 'cat', 'horse'], ['frog', 'turtle', 'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]
